376a316c97a0736124d3d1e4229d4416fdcb53a7
Gerardo Lucas Cruz <gerardo.lucas@accenture.com>
2023-02-27 12:46:29 -0600

    Initial project

---
+++ src/main/java/com/geezylucas/introductionspringbatch/IntroductionSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.geezylucas.introductionspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionSpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geezylucas/introductionspringbatch/IntroductionSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geezylucas.introductionspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
